42bb3da951b5f653b783174c343d4cf8f2aa2144
Sergey Nemov <sergey.n.nemov@gmail.com>
2019-02-10 18:39:53 +0100

    sssnake: add two players support
    Just a basic support for 2 players. Snakes can go through each other for
    now.

--- sssnake.c
+++ sssnake.c
@@ -10,7 +10,8 @@
 #define SNAKE_END_SCORE     10
 
 char *snake_menu[] = {
-                        "Play",
+                        "One Player",
+                        "Two Players",
                         "Exit",
 };
 
@@ -20,18 +21,19 @@ struct snake_coord {
 
 struct snake {
     struct snake_coord body[SNAKE_MAX_LENGTH];
-    int len;
+    int score, color;
+    int caught, len;
 };
 
 struct snake_game {
-    int food_x, food_y, caught;
+    int food_x, food_y;
     int speed_inc, speed;
     int next_x, next_y;
     int max_x, max_y;
-    int score, color;
     int endscore;
 
-    struct snake snake;
+    struct snake snake1;
+    struct snake snake2;
 };
 
 void snake_exit(const char *message)
@@ -56,8 +58,8 @@ int snake_collision(struct snake *snake)
 
     /* Check if snake intersects its body */
     for (int i = 1; i < snake->len; i++) {
-           if (head_x == snake->body[i].x &&
-           head_y == snake->body[i].y)
+        if (head_x == snake->body[i].x &&
+            head_y == snake->body[i].y)
             return 1;
     }
 
@@ -72,17 +74,19 @@ int snake_collision(struct snake *snake)
 
 void snake_init_snake(struct snake *snake)
 {
+    int max_y, max_x;
+
+    getmaxyx(stdscr, max_y, max_x);
+
     for (int i = 1; i < SNAKE_MAX_LENGTH; i++) {
            snake->body[i].x = -10;
            snake->body[i].y = -10;
     }
 
-    snake->body[0].x = 0;
-    snake->body[0].y = 0;
+    snake->body[0].x = rand() % max_x;
+    snake->body[0].y = rand() % max_y;
 
-    /* We have to know the coordinates for new body */
-    snake->body[1].x = 0;
-    snake->body[1].y = 0;
+    snake->score = 0;
     snake->len = 1;
 }
 
@@ -98,9 +102,12 @@ void snake_move(struct snake *snake, int enlarge, int direction_x, int direction
     }
 
     /* move the body */
-    for (; i > 0; i--) {
-        snake->body[i].x = snake->body[i-1].x;
-        snake->body[i].y = snake->body[i-1].y;
+    /* make sure we don't move the body if there's no direction */
+    if (!(direction_x == 0 && direction_y == 0)) {
+        for (; i > 0; i--) {
+            snake->body[i].x = snake->body[i-1].x;
+            snake->body[i].y = snake->body[i-1].y;
+        }
     }
 
     /* move the head */
@@ -112,7 +119,7 @@ void snake_move(struct snake *snake, int enlarge, int direction_x, int direction
 }
 
 
-void snake_print(struct snake *snake)
+void snake_draw(struct snake *snake)
 {
     /* Print the body */
     for (int i = snake->len-1; i > 0; i--) {
@@ -172,37 +179,35 @@ void snake_init_screen()
     }
 }
 
-void snake_game_init(struct snake_game *game)
+void snake_game_init(struct snake_game *game, bool twoplayers)
 {
     game->endscore = SNAKE_END_SCORE;
-    game->score = 0;
-    /* To make the food appear ?? */
-    game->caught = 1;
     game->speed = SNAKE_DELAY_MAX;
     /* Make the speed increasing up to 3/4 of the game's end score */
     game->speed_inc = (SNAKE_DELAY_MAX - SNAKE_DELAY_MIN)/(3*game->endscore/4);
 
     /* Setup snake */
-    snake_init_snake(&game->snake);
+    snake_init_snake(&game->snake1);
+    if (twoplayers)
+        snake_init_snake(&game->snake2);
 
     /* Setup ncurses stuff */
     snake_init_screen();
 }
 
-/*
- * 258 KEY_DOWN
- * 259 KEY_UP
- * 260 KEY_LEFT
- * 261 KEY_RIGHT
- */
-void snake_run(struct snake_game *game)
+void snake_run(struct snake_game *game, bool twoplayers)
 {
-    int direction_x = 1, direction_y = 0;
-    struct snake *snake = &game->snake;
-    int next_x = 0, next_y = 0;
+    int direction1_x = 0, direction1_y = 0;
+    int direction2_x = 0, direction2_y = 0;
+    struct snake *snake1 = &game->snake1;
+    struct snake *snake2 = &game->snake2;
+    int next1_x = 0, next1_y = 0;
+    int next2_x = 0, next2_y = 0;
     int max_x = 0, max_y = 0;
-    int *head_x = &snake->body[0].x;
-    int *head_y = &snake->body[0].y;
+    int *head1_x = &snake1->body[0].x;
+    int *head1_y = &snake1->body[0].y;
+    int *head2_x = &snake2->body[0].x;
+    int *head2_y = &snake2->body[0].y;
 
     while(1) {
         int c = getch();
@@ -210,32 +215,66 @@ void snake_run(struct snake_game *game)
         switch(c) {
             case KEY_LEFT:
                 /* Check it's not RIGHT, we can't turn the opposite direction */
-                if (direction_x != 1 && direction_y != 0) {
-                    direction_x = -1;
-                    direction_y = 0;
+                if (!(direction1_x == 1 && direction1_y == 0)) {
+                    direction1_x = -1;
+                    direction1_y = 0;
                 }
                 break;
             case KEY_RIGHT:
                 /* Check it's not LEFT, we can't turn the opposite direction */
-                if (direction_x != -1 && direction_y != 0) {
-                    direction_x = 1;
-                    direction_y = 0;
+                if (!(direction1_x == -1 && direction1_y == 0)) {
+                    direction1_x = 1;
+                    direction1_y = 0;
                 }
                 break;
             case KEY_UP:
                 /* Check it's not DOWN, we can't turn the opposite direction */
-                if (direction_x != 0 && direction_y != 1) {
-                    direction_x = 0;
-                    direction_y = -1;
+                if (!(direction1_x == 0 && direction1_y == 1)) {
+                    direction1_x = 0;
+                    direction1_y = -1;
                 }
                 break;
             case KEY_DOWN:
                 /* Check it's not UP, we can't turn the opposite direction */
-                if (direction_x != 0 && direction_y != -1) {
-                    direction_x = 0;
-                    direction_y = 1;
+                if (!(direction1_x == 0 && direction1_y == -1)) {
+                    direction1_x = 0;
+                    direction1_y = 1;
                 }
                 break;
+        }
+
+        if (twoplayers) {
+            /* using wasd keys*/
+            switch(c) {
+                case 97: /* a - LEFT */
+                    /* Check it's not RIGHT, we can't turn the opposite direction */
+                    if (!(direction2_x == 1 && direction2_y == 0)) {
+                        direction2_x = -1;
+                        direction2_y = 0;
+                    }
+                    break;
+                case 100: /* d - RIGHT */
+                    /* Check it's not LEFT, we can't turn the opposite direction */
+                    if (!(direction2_x == -1 && direction2_y == 0)) {
+                        direction2_x = 1;
+                        direction2_y = 0;
+                    }
+                    break;
+                case 119: /* w - UP */
+                    /* Check it's not DOWN, we can't turn the opposite direction */
+                    if (!(direction2_x == 0 && direction2_y == 1)) {
+                        direction2_x = 0;
+                        direction2_y = -1;
+                    }
+                    break;
+                case 115: /* s - DOWN */
+                    /* Check it's not UP, we can't turn the opposite direction */
+                    if (!(direction2_x == 0 && direction2_y == -1)) {
+                        direction2_x = 0;
+                        direction2_y = 1;
+                    }
+                    break;
+            }
         }
 
         /* exit game 113 == 'q' */
@@ -243,11 +282,13 @@ void snake_run(struct snake_game *game)
             break;
 
         /* set the color */
-        if (c != -1 && c != game->color &&
+        /*
+        if (c != -1 && c != snake1->color &&
             c != KEY_LEFT && c != KEY_RIGHT &&
             c != KEY_UP && c != KEY_DOWN)
-            game->color = c;
-        attrset(COLOR_PAIR(game->color % 8));
+            snake1->color = c;
+        attrset(COLOR_PAIR(snake1->color % 8));
+        */
 
         /* Global var `stdscr` is created by the call to `initscr()` */
         getmaxyx(stdscr, max_y, max_x);
@@ -256,50 +297,81 @@ void snake_run(struct snake_game *game)
         clear();
 
         /* print the score */
-        mvprintw(max_y/2 - 3, max_x/2, "Score: %i", game->score);
+        mvprintw(max_y/2 - 3, max_x/2, "Player1 Score: %i", snake1->score);
+        if (twoplayers)
+            mvprintw(max_y/2 - 4, max_x/2, "Player2 Score: %i", snake2->score);
 
         /* print the debug information */
-        mvprintw(max_y/2 - 2, max_x/2, "X: %i", *head_x);
-        mvprintw(max_y/2 - 1, max_x/2, "Y: %i", *head_y);
-        mvprintw(max_y/2, max_x/2, "dirX: %i", direction_x);
-        mvprintw(max_y/2 + 1, max_x/2, "dirY: %i", direction_y);
+        mvprintw(max_y/2 - 2, max_x/2, "X: %i", *head1_x);
+        mvprintw(max_y/2 - 1, max_x/2, "Y: %i", *head1_y);
+        mvprintw(max_y/2, max_x/2, "dirX: %i", direction1_x);
+        mvprintw(max_y/2 + 1, max_x/2, "dirY: %i", direction1_y);
         mvprintw(max_y/2 + 2, max_x/2, "MaxX: %i", max_x);
         mvprintw(max_y/2 + 3, max_x/2, "MaxY: %i", max_y);
         mvprintw(max_y/2 + 4, max_x/2, "Char: %i", c);
         mvprintw(max_y/2 + 5, max_x/2, "Speed: %i", game->speed);
         mvprintw(max_y/2 + 6, max_x/2, "SpInc: %i", game->speed_inc);
+
         /* Print food at the current xy position */
         mvprintw(game->food_y, game->food_x, "o");
 
-        snake_print(snake);
+        if (twoplayers) {
+            snake_draw(snake1);
+            snake_draw(snake2);
+        } else {
+            snake_draw(snake1);
+        }
+
         refresh();
 
         /* Shorter delay between movements */
         usleep(game->speed);
 
-        next_x = *head_x + direction_x;
-        next_y = *head_y + direction_y;
+        next1_x = *head1_x + direction1_x;
+        next1_y = *head1_y + direction1_y;
+        if (twoplayers) {
+            next2_x = *head2_x + direction2_x;
+            next2_y = *head2_y + direction2_y;
+        }
 
-        /* snake will got the food */
-        if (next_x == game->food_x && next_y == game->food_y) {
-            game->caught = 1;
-            game->score++;
+        /* snake1 will got the food */
+        if (next1_x == game->food_x && next1_y == game->food_y) {
+            snake1->caught = 1;
+            snake1->score++;
             snake_increase_speed(game);
         }
 
-        snake_move(snake, game->caught, direction_x, direction_y);
+        /* snake2 will got the food */
+        if (twoplayers && (next2_x == game->food_x && next2_y == game->food_y)) {
+            snake2->caught = 1;
+            snake2->score++;
+            snake_increase_speed(game);
+        }
+
+        snake_move(snake1, snake1->caught, direction1_x, direction1_y);
+        if (twoplayers)
+            snake_move(snake2, snake2->caught, direction2_x, direction2_y);
 
         /* Generate a new place for food */
-        if (game->caught) {
+        if (snake1->caught) {
             srand(time(NULL));
             game->food_x = rand() % max_x;
             game->food_y = rand() % max_y;
-            game->caught = 0;
+            snake1->caught = 0;
         }
 
-        if (game->score >= game->endscore)
-            snake_exit("You won!");
+        if (twoplayers && snake2->caught) {
+            srand(time(NULL));
+            game->food_x = rand() % max_x;
+            game->food_y = rand() % max_y;
+            snake2->caught = 0;
+        }
 
+        if (snake1->score >= game->endscore) {
+            snake_exit(twoplayers ? "Player1 won!" : "You won!");
+        } else if (snake2->score >= game->endscore) {
+            snake_exit("Player2 won!");
+        }
     }
 
     /* Restore normal terminal behavior */
@@ -316,7 +388,7 @@ void snake_start_menu()
     snake_init_screen();
 
     getmaxyx(stdscr, max_y, max_x);
-    WINDOW *menuwin = newwin(4, 20, max_y/2 - 2, max_x/2 - 10);
+    WINDOW *menuwin = newwin(5, 20, max_y/2 - 2, max_x/2 - 10);
     box(menuwin, 0, 0);
     refresh();
     wrefresh(menuwin);
@@ -324,10 +396,10 @@ void snake_start_menu()
     keypad(menuwin, true);
 
     while (1) {
-        highlight %= 2;
+        highlight %= 3;
         clear();
 
-        for (i = 0; i < 2; i++) {
+        for (i = 0; i < 3; i++) {
             if (i == highlight)
                 wattron(menuwin, A_REVERSE);
             mvwprintw(menuwin, i + 1, 1, snake_menu[i]);
@@ -361,16 +433,23 @@ void snake_start_menu()
 
     delwin(menuwin);
 
-    if (highlight == 0) {
+    if (highlight == 0 || highlight == 1) {
         struct snake_game game;
+        int twoplayers;
 
-        snake_game_init(&game);
-        snake_run(&game);
-    } else if (highlight == 1) {
+        if (highlight == 0)
+            twoplayers = 0;
+        else
+            twoplayers = 1;
+
+        snake_game_init(&game, twoplayers);
+        snake_run(&game, twoplayers);
+
+    } else if (highlight == 2) {
         snake_exit("Bye!");
     }
 
-    return;
+    snake_exit("Bye!");
 }
 
 int main(int argc, char *argv[])
